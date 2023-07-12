ee7efb2840655832f2bfcfab7ab51f8f3ff49d9f
StefaanDhondt <stefaandh@gmail.com>
2023-07-12 21:12:32 +0200

    autoreconnect after ws connection is lost + cleanup.

--- static/Utils.js
+++ static/Utils.js
@@ -2,32 +2,35 @@ class Sender
 {
   constructor(host)
   {
-    this.socket = new WebSocket('ws://' + host);
-    //var ws = this;
-    this.queue = []
-    this.socket.onopen = function()
-    {
-      while (this.queue.length > 0) 
-      {
-        this.socket.send(this.queue.pop());
-      }
-    }.bind(this);
+    this.host = host;
+    this.socket = null;
+    this.queue = [];
   }
 
   send(input)
   {
     let input_as_string = JSON.stringify(input);
-    if (this.socket.readyState === WebSocket.OPEN)
-    {
-      this.socket.send(input_as_string);
-    }
-    else
+    if (!this.socket || this.socket.readyState !== WebSocket.OPEN) 
     {
       this.queue.push(input_as_string);
-    }
+      if (!this.socket || this.socket.readyState !== WebSocket.CONNECTING)
+      {
+        this.socket = new WebSocket('ws://' + this.host);
+        this.socket.onopen = function()
+        {
+          while (this.queue.length > 0) 
+          {
+            this.socket.send(this.queue.pop());
+          }
+        }.bind(this);
+      }
+    } 
+    else 
+    {
+      this.socket.send(input_as_string);
+    }  
   }
 }
-
 
 function send_ir_event(remote, code, sender)
 {
