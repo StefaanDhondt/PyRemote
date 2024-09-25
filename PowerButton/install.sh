1e3db859e088ca884ce1e8bd6a87e6f23603e8cb
StefaanDhondt <stefaandh@gmail.com>
2024-09-25 21:32:25 +0200

    Add power button documentation

--- PowerButton/install.sh
+++ PowerButton/install.sh
@@ -1,3 +1,11 @@
+# Need to run this once:
+# sudo apt-get install python3-rpi-lgpio
+# otherwise:
+# Traceback (most recent call last):
+#   File "/usr/local/bin/listen-for-shutdown.py", line 9, in <module>
+#     GPIO.wait_for_edge(3, GPIO.FALLING)
+# RuntimeError: Error waiting for edge
+
 sudo cp listen-for-shutdown.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/listen-for-shutdown.py
 
